bc3c0139a0d364b687969f512f126c2917510313
Josh Rabideaux <jrabidea@dhcp-11-19.bravo>
2015-07-09 12:03:19 -0500

    Test

--- blackjack.py
+++ blackjack.py
@@ -8,6 +8,9 @@ print "*********************************"
 players_hand = []
 dealers_hand = []
 discard_pile = []
+play = True
+deal = True
+players_turn = False
 
 card_deck = {
 	
@@ -65,9 +68,86 @@ card_deck = {
 	"Kh" : 10
 }
 
-suite = ["s", "c", "h", "d"]
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-rand_num_card = randrange(0, 10, 1)
-rand_num_suite = randrange(0, 4, 1)
+def generate_card():
 
-print str(numbers[rand_num_card]) + suite[rand_num_suite]
+	retry = True
+	suite = ["s", "c", "h", "d"]	
+	numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+	rand_num_card = randrange(0, 13, 1)
+	rand_num_suite = randrange(0, 4, 1)
+	while retry:
+
+		if numbers[rand_num_card] == 11:
+				card = "J" + suite[rand_num_suite]
+				retry = False
+		elif numbers[rand_num_card]  == 12:
+			card = "Q" + suite[rand_num_suite]
+			retry = False
+		elif numbers[rand_num_card] == 13:
+			card = "K" + suite[rand_num_suite]
+			retry = False
+		elif numbers[rand_num_card] == 1:
+			card = "A" + suite[rand_num_suite]
+			retry = False
+		else:
+			card = str(numbers[rand_num_card]) + suite[rand_num_suite]
+			retry = False
+
+		if len(discard_pile) > 0:
+			for d in discard_pile:
+				for key in card_deck:
+					if key == card:
+						retry = True
+					else:
+						retry = False
+		else:
+				retry = False
+
+	return card
+
+def generate_players_hand():
+
+	deal_player = True	
+
+	while deal_player:
+		deal_player = False
+		card = generate_card()
+		if len(players_hand) == 0:
+			players_hand.append(card)
+			deal_player = True
+		elif len(players_hand) > 0 and len(players_hand) <= 2:
+				for a in players_hand:
+					if card == a:
+						deal_player = True
+				players_hand.append(card)
+
+	print players_hand
+
+def generate_dealers_hand():
+
+	deal_dealer = True
+
+	while deal_dealer:
+		deal_dealer = False
+		card = generate_card()
+		if len(dealers_hand) == 0:
+			if card == players_hand[0] or card == players_hand[1]:
+				deal_dealer = True
+			else:
+				dealers_hand.append(card)
+				deal_dealer = True
+		elif len(dealers_hand) > 0 and len(dealers_hand) <= 2:
+			if card == players_hand[0] or card == players_hand[1]:
+				deal_dealer = True
+			else:
+				dealers_hand.append(card)
+
+	print dealers_hand
+
+
+generate_players_hand()
+generate_dealers_hand()
+
+
+	
+
